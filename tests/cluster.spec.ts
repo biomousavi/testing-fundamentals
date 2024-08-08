f832a913793a4184baffa713469af860d4873626
Mohammad Hosein Mousavi <biomousavi@gmail.com>
2024-08-08 18:09:02 +0330

    test: prints current parameters

--- tests/cluster.spec.ts
+++ tests/cluster.spec.ts
@@ -5,3 +5,10 @@ test("has correct title", async ({ page }) => {
 
   await expect(page).toHaveTitle("Chicago Traffic Accidents Clustering");
 });
+
+test("it prints current parameters", async ({ page }) => {
+  await page.goto("http://localhost:5173/clustering/?size=1000&distance=450");
+
+  await expect(page.locator("span.size")).toHaveText("1000");
+  await expect(page.locator("span.distance")).toHaveText("450");
+});
