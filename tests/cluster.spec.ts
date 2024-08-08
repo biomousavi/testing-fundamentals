9dd469089ab1645907d82341e8e532a525954369
Mohammad Hosein Mousavi <biomousavi@gmail.com>
2024-08-08 18:25:08 +0330

    refactor: cluster page e2e test

--- tests/cluster.spec.ts
+++ tests/cluster.spec.ts
@@ -1,14 +1,47 @@
-import { test, expect } from "@playwright/test";
+import { test, expect, Page } from "@playwright/test";
 
 test("has correct title", async ({ page }) => {
-  await page.goto("http://localhost:5173/clustering/");
+  const cluster = new ClusterPage(page);
 
-  await expect(page).toHaveTitle("Chicago Traffic Accidents Clustering");
+  await cluster.goto();
+
+  await expect(cluster.title).resolves.toMatch(
+    "Chicago Traffic Accidents Clustering"
+  );
 });
 
 test("it prints current parameters", async ({ page }) => {
-  await page.goto("http://localhost:5173/clustering/?size=1000&distance=450");
+  const size = 1000;
+  const distance = 540;
 
-  await expect(page.locator("span.size")).toHaveText("1000");
-  await expect(page.locator("span.distance")).toHaveText("450");
+  const cluster = new ClusterPage(page);
+  await cluster.goto(size, distance);
+
+  await expect(cluster.size).toHaveText(size.toString());
+  await expect(cluster.distance).toHaveText(distance.toString());
 });
+
+class ClusterPage {
+  constructor(private page: Page) {}
+
+  async goto(size = 1000, distance = 450) {
+    const url = new URL("http://localhost:5173/clustering/");
+
+    if (size) url.searchParams.set("size", size.toString());
+    if (distance) url.searchParams.set("distance", distance.toString());
+
+    await this.page.goto(url.toString());
+  }
+
+  get title() {
+    return this.page.title();
+  }
+
+  get size() {
+    return this.page.locator("span.size");
+  }
+
+  get distance() {
+    return this.page.locator("span.distance");
+  }
+}
